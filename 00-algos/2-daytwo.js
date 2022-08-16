0a90c2256ea16014412094271758ba0f721bd349
sdahal1 <raabdahl@gmail.com>
2022-08-16 09:16:43 -0700

    added algos tuesday

---
+++ 00-algos/2-daytwo.js
@@ -0,0 +1,84 @@
+var amazingShow1 = "Curb your enthusiasm is #1 ! yay :) "; //this is a variable that is equal to a string
+var magicsNumber = 32.01; //32 is an number
+
+
+amazingShow1 = "The office"; //this is an example of reassigning the variable amazingShow1 to be equal to "the office"
+
+var isRobSpeakingFactsAboutBasketball = false; //booleans are another data type: true or false
+
+
+
+//a data type is a simple piece of information such as a string, number, boolean
+
+
+//a data structure is something that can store multiple data types (arrays, objects(hash maps), trees, linked lists....etc;)
+
+
+//an array is a data structure where you can group multiple pieces of information together 
+
+var greatestArtists = ["Drake", "J Cole", "Kendrick", "Kanye", "Tupac", "Beyonce", "Taylor Swift", "Lupe Fiasco"] //array of string
+//indexes (positions)     0         1         2         3         4       5            6                 7
+
+//actual values you see in the array are called: ELEMENTS
+
+var randomNums = [23, 0, 77, 12, 8]; //array of numbers
+
+var mixedArray = ["Potato", 5, false, true, "hello", 9]; //this is a mixed array- mixed data types
+
+
+// console.log("wazzaaaap");
+
+// console.log(greatestArtists[0])
+// console.log(greatestArtists[1])
+// console.log(greatestArtists[2])
+// console.log(greatestArtists[3])
+// console.log(greatestArtists[4])
+// console.log(greatestArtists[5]) //print the greatestArtists at index number 5
+// console.log(greatestArtists[6])
+// console.log(greatestArtists)
+
+// for(var i = 0; i<greatestArtists.length; i++){
+//     console.log(greatestArtists[i])
+// }
+
+
+
+
+//a loop is used to do something repeatedly.Example-  print every number from 1 to 100
+
+// for(var num = 1; num<=100; num++){
+//     //this is the body of the for loop
+//     console.log(num)
+// }
+
+//print from 10 - 1
+// for(var num = 10; num>=1; num--){
+//     //this is the body of the for loop
+//     console.log(num)
+// }
+
+// console.log("blast off")
+
+
+
+// var a = 25;
+// a = a - 13;
+// console.log(a/2);
+    
+// a = "hello";
+// console.log(a + " hello");
+
+
+for(var i=0; i<10; i++) {
+    console.log(i);
+    i = i + 3; 
+}
+    
+console.log("outside of the loop " + i);
+
+
+
+
+
+
+
