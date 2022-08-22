fa2861ba5ecbc6f5dae4e55e3782f37f29da2c19
sdahal1 <raabdahl@gmail.com>
2022-08-22 14:20:02 -0700

    puppy gram complete

--- 03-week3/puppyGram/script.js
+++ 03-week3/puppyGram/script.js
@@ -27,3 +27,49 @@ function removeButton(btn){
     // console.log("removing something!", btn);
     btn.remove();
 }
+
+function playVideo(videoElement){
+    // console.log("annnnd action!");
+    videoElement.play();
+}
+
+function pauseVideo(videoElement){
+    // console.log("annnnd action!", videoElement);
+    videoElement.pause();
+}
+
+function switchImg(imgElement){
+    console.log(imgElement.src);
+    if(imgElement.src === "http://127.0.0.1:5501/03-week3/puppyGram/resources/puppy.jpeg"){
+        imgElement.src = "./resources/anothaPuppy.jpeg"
+    }else{
+        imgElement.src = "./resources/puppy.jpeg"
+    }
+}
+
+
+function removeCookieBox(id){
+    // console.log('id of the box i want to remove is ', id)
+    //grab from the html an element with the id of "cookie-box" and put it in a variable
+    let elementToRemove = document.querySelector("#"+id);
+    elementToRemove.remove();
+}
+
+
+function increaseLikes(elementID){
+    console.log("i liike", elementID);
+    //grab an element with that elementID
+    let spanElement = document.querySelector("#"+ elementID);
+    console.log(spanElement.innerText)
+
+    //store the number that is in the span element into a variable
+    let currentCount = spanElement.innerText;
+
+    //increment the count
+    currentCount++;
+
+    //update the spanElement inner text to be the current count after increment the current count
+    spanElement.innerText = currentCount;
+
+    // document.querySelector("#"+elementID).innerText =  parseInt(document.querySelector("#"+elementID).innerText)+1;
+}
