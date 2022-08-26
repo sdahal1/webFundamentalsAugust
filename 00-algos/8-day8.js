b57a8f7bb8a31539d85390b84e04a3851e83f113
sdahal1 <raabdahl@gmail.com>
2022-08-26 08:47:30 -0700

    nested arrays

---
+++ 00-algos/8-day8.js
@@ -0,0 +1,49 @@
+let nums = [23, 0, 11, 32, 8];
+let artists = ["Mariah Carey", "Elvis", "Nipsey", "J cole", "Taylor Swift"];
+
+let mixedArr = [10, "word", false];
+
+
+let array2d = [
+    [2,5,8,10], //0
+    [3,6,1,11], //1
+    [5,7,7,99] //2
+]
+
+
+
+// console.log(array2d[1][1])
+
+// console.log(array2d[2][0])
+
+
+
+// for(let i = 0; i<array2d.length; i++){
+//     for(let j = 0; j<array2d[i].length; j++){
+//         console.log(array2d[i][j])
+//     }
+
+
+// }
+
+/*
+i = 0
+    j = 0, 1, 2, 3, 4
+i = 1
+    j = 0, 1, 2, 3, 4, 
+i = 2
+    j = 0, 1, 2, 3, 4,
+i = 3 
+
+
+
+
+*/
+
+
+
+// for(let minuteHand = 0; minuteHand<10; minuteHand++){
+//     for(let secondHand = 0; secondHand< 60; secondHand++){
+//         console.log(`Minute: ${minuteHand}- Second: ${secondHand}`)
+//     }
+// }
--- 03-week3/apiLecture/script.js
+++ 03-week3/apiLecture/script.js
@@ -29,6 +29,4 @@ async function getMoney(){
         div.appendChild(pTag);
 
     }
-
-    
 }
